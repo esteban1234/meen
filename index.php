82fbfbda69aa6b7ae44a69a6e63602f3d8919f73
esteban1234 <tva_libra@hotmail.com>
2016-11-23 12:13:27 -0600

    avance del index

--- index.php
+++ index.php
@@ -3,20 +3,34 @@
 
 
   <body>
-    <div class="portada">
+
+    <section class="portada">
+      <article class="reglacabeza">
+        <article class="nombrecabeza">
+          <span class="icon-text-document"></span>&nbsp;&nbsp;CONSTRUCTORA
+        </article>
+         <article class="nombrecabeza2">
+          <span class="icon-mail"></span>&nbsp;&nbsp;<b>MAIL:</b>&nbsp;&nbsp;PENDIENTE
+        </article>
+      </article>
+      <figure>
+      <img src="img/MEEN.png" alt="" width="350" height="300">
+      </figure>
       <!-- <div class="text">
         <h1> div con imagen de fondo responsive</h1>
         <h3>Imagen adaptada a todas las resoluciones de pantalla</h3>
       </div> -->
-    </div>
+    </section>
+
+    <!-- <img src="img/MEEN.jpg" alt=""> -->
 
 
-      <span class="ir-arriba icon-arrow-bold-up"></span>
+      <!-- <span class="ir-arriba icon-arrow-bold-up"></span> -->
 
-      <?php include 'vistas/pie.php';?>
+      <!-- <?php include 'vistas/pie.php';?> -->
 
 
-    <script type="text/javascript" src="js/jquery.js"></script>
-    <script src="js/menu.js"></script>
-    <script type="text/javascript" src="js/iconoarriba.js"></script>
+    <!-- <script type="text/javascript" src="js/jquery.js"></script> -->
+    <!-- <script src="js/menu.js"></script> -->
+    <!-- <script type="text/javascript" src="js/iconoarriba.js"></script> -->
   </body>
