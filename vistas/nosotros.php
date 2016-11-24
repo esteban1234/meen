66f3321cbd437c2648df7098cc33e628a6e12d33
esteban1234 <tva_libra@hotmail.com>
2016-11-24 12:49:42 -0600

    quines somos cont

--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -24,12 +24,26 @@
       <h1 class="contacto">Acerca de nosotros, Constructora <b class="meen">MEEN</b></h1>
 
       <article class="textonos">
-        <p class="textonoss">Constructora <b class="meen">MEEN</b>  es una empresa especializada en el desarrollo de conjuntos habitacionales, inmuebles comerciales, institucionales y de servicios, con una gran variedad de proyectos construidos. Es una de las principales constructoras, porque ofrecemos infraestructura acompañados con el sector educativo, comercial y recreativo. <br><br>
+        <p class="textonoss">Constructora <b class="meen">MEEN</b>  es una empresa especializada en el desarrollo de conjuntos habitacionales, inmuebles comerciales, institucionales y de servicios, con una gran variedad de proyectos construidos. Somos una de las principales constructoras, porque ofrecemos infraestructura acompañados con el sector educativo, comercial y recreativo. <br><br>
         Satisfacemos las necesidades de vivienda de todos los segmentos, desde vivienda de subsidio, de interés social, medio y residencial, ofreciendo siempre la más alta calidad de construcción y atención a sus clientes.</p>
 
       </article>
 
       <article class="puntosmv">
+
+        <article class="puntos">
+          <p>¿PORQUE CONSTRUIR CON NOSOTROS?</p>
+          <p>Porque ofrece:</p>
+          <p>Más de 40 años de experiencia.</p>
+          <p>Excelente relación precio/metro cuadrado.</p>
+          <p>El mejor equipamiento en sus desarrollos.</p>
+          <p>Excelentes ubicaciones y vías de acceso.</p>
+          <p>La mayor plusvalía.</p>
+          <p>Trámites gratuitos.</p>
+          <p>Asesores certificados.</p>
+          <p>Calidad garantizada.</p>
+        </article>
+        <article class="mv">aqui va lo de mision vision y objetivos </article>
 
       </article>
 
