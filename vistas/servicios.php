89015a0db0e06c76475359279e2c816e33e450f4
esteban1234 <tva_libra@hotmail.com>
2016-11-25 12:01:50 -0600

    servicios lista

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -33,14 +33,86 @@
 
         <article class="text_serv">
         <p class="text_servenca">Proyectos Arquitectónicos</p>
-        <p class="text_sub">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A molestias quos, asperiores temporibus, iste distinctio quidem ut non perspiciatis quaerat officiis laborum magni fugit nihil incidunt voluptatibus nulla, adipisci eveniet.</p>
+        <p class="text_sub">En CONSTRUCTORA <b class="meen">MEEN</b>, sabemos la importancia de su construcción, contamos con arquitectos especializados en planos arquitectónicos, para que su desarrollo sea de total calidad y gusto del cliente antes durante y despues de cada proyecto.</p>
+        <p class="text_sub">• Construcciones Funcionales. <br>
+        • Optimización de Áreas. <br>
+        • Instalaciones para todo tipo de proyecto.<br>
+        • Ampliaciones.<br>
+        • Acabados estéticos.<br>
+        </p>
         </article>
 
         <figure class="figura">
-          <img src="" alt="">
+          <img src="../img/arquitectura-construccion.jpg" alt="" width="300" height="250">
         </figure>
 
 
+      </article>
+
+       <article class="contservicioss">
+
+          <article class="text_serv">
+          <p class="text_servenca">Estudios preliminares</p>
+          <p class="text_sub">Analizamos, interpretamos, utilizamos y coordinamos los estudios preliminares de obra, proporcionando los datos e información que te permitirán conocer las características y condiciones del terreno a construir. Con base a la interpretación y análisis te ayudamos determinar el tipo de documentación y procesos constructivos a seguir.</p>
+          <p class="text_sub">• Geotecnia. <br>
+          • Topografía. <br>
+          • Hidrología.<br>
+          </p>
+          </article>
+
+          <figure class="figura">
+            <img src="../img/medicion.jpg" alt="" width="300" height="250">
+          </figure>
+      </article>
+
+      <article class="contservicioss">
+
+          <article class="text_serv">
+          <p class="text_servenca">Terracerias</p>
+          <p class="text_sub">Se realiza con el mayor factor de seguridad, con pruebas de laboratorio para verificar pesos volumétricos, contenido de humedad y porcentajes de compactación requeridos, según el diseño y cálculos previamente establecidos.</p>
+          <p class="text_sub">• Cortes. <br>
+          • Rellenos. <br>
+          • Nivelaciones.<br>
+          • Mejoramiento de Suelos.<br>
+          </p>
+          </article>
+
+          <figure class="figura">
+            <img src="../img/terraceria.jpg" alt="" width="300" height="250">
+          </figure>
+      </article>
+
+      <article class="contservicioss">
+
+          <article class="text_serv">
+          <p class="text_servenca">Cimientos Especiales</p>
+          <p class="text_sub">Diseñamos, calculamos y ejecutamos cimentaciones especiales de acuerdo a las necesidades de su construcción, siguiendo los más altos estándares de seguridad e implementando ingeniería de vanguardia.</p>
+          <p class="text_sub">• Cimentación de prensas. <br>
+          •Cimentación para silos o equipos de alto tonelaje ya sean estáticos ó dinámicos. <br>
+          </p>
+          </article>
+
+          <figure class="figura">
+            <img src="../img/cimiento.jpg" alt="" width="300" height="250">
+          </figure>
+      </article>
+
+      <article class="contservicioss">
+
+          <article class="text_serv">
+          <p class="text_servenca">Edificación</p>
+          <p class="text_sub">Contamos con la asesoría técnica y profesional para la realización de proyecto y Construcción de edificios de cualquier giro que sea su empresa.</p>
+          <p class="text_sub">• Edificaciones para Oficinas. <br>
+          • Edificios Comerciales. <br>
+          • Edificios para Baños. <br>
+          • Cuartos Tipo Industrial. <br>
+          • Casetas de Vigilancia. <br>
+          </p>
+          </article>
+
+          <figure class="figura">
+            <img src="../img/edificacion.jpg" alt="" width="300" height="250">
+          </figure>
       </article>
 
       </article>
